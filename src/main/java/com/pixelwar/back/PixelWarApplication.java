c1fe7bb876d36303c176cec4ae342cf2a51ba1d2
Guilsata <guilsayan17@gmail.com>
2022-06-12 09:55:50 +0200

    creation project

---
+++ src/main/java/com/pixelwar/back/PixelWarApplication.java
@@ -0,0 +1,13 @@
+package com.pixelwar.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixelwar/back/PixelWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelwar.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
